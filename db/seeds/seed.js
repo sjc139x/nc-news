7be15fd286b422f7ce3a84eb1e717216e1caa418
Samantha Cockerill <samanthajaynecockerill@gmail.com>
2019-04-02 11:24:44 +0100

    seed function for users table now working

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,25 @@
+const { userData, topicData, articleData, commentData } = require('../data');
 
 exports.seed = function(knex, Promise) {
   return knex.migrate.rollback()
   .then(() => knex.migrate.latest())
-  .then(() => console.log('Seed function doing something...'));
+  .then(() => knex('users').insert(userData).returning('*'))
+  .then((res) => console.log(res));
 };
+
+
+
+// exports.seed = function(knex, Promise) {
+//     return knex.migrate
+//       .rollback()
+//       .then(() => knex.migrate.latest())
+//       .then(() =>  knex('houses').insert(houseData).returning('*'))
+//       .then(houseRows => {
+//         const houseRef = createRef(houseRows, 'house_name', 'house_id');
+//         const formattedWizards = formatWizards(wizardData, houseRef);
+//         const wizardInsertions = knex('wizards')
+//           .insert(formattedWizards)
+//           .returning('*');
+//         return Promise.all([houseRows, wizardInsertions]);
+//       });
+//   };
