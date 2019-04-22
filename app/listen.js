d75197084da4c72381530bc7aa75ec13a0dbe38d
Samantha Cockerill <samanthajaynecockerill@gmail.com>
2019-04-22 21:17:08 +0100

    adding hosting logic

--- app/listen.js
+++ app/listen.js
@@ -1,5 +1,8 @@
 const app = require('./app');
+const PORT = process.env.PORT || 3030;
 
-app.listen(3030, () => {
-    console.log('Listening on port 3030...')
+//don't understand...
+app.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`Listening on port ${PORT}...`);
 });
--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const dbConfig = require('../knexfile');
+const { iam_apikey } = process.env.NODE_ENV === 'production' ? process.env : require('../knexfile');
 
-const connection = knex(dbConfig);
+const connection = knex(iam_apikey);
 
 module.exports = connection;
