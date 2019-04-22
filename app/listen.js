375e50afb7c7cd15e4822828c7856c29bf6cddd7
Samantha Cockerill <samanthajaynecockerill@gmail.com>
2019-04-22 21:46:09 +0100

    hosting

--- app/listen.js
+++ app/listen.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3030;
 
-//don't understand...
 app.listen(PORT, err => {
     if (err) throw err;
     console.log(`Listening on port ${PORT}...`);
--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 const knex = require('knex');
-const dbConfig = require('../knexfile');
-const { iam_apikey } = process.env.NODE_ENV === 'production' ? process.env : require('../knexfile');
+const iam_apikey = process.env.NODE_ENV === 'production' ? process.env : require('../knexfile');
 
 const connection = knex(iam_apikey);
 
