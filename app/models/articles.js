4eff8538dfca4276ec980aa972743c2bd8dfbf1d
Samantha Cockerill <samanthajaynecockerill@gmail.com>
2019-06-02 13:26:09 +0100

    updating articles model to allow image field

--- app/models/articles.js
+++ app/models/articles.js
@@ -1,78 +1,113 @@
-const connection = require('../../db/connection');
+const connection = require("../../db/connection");
 
-function fetchArticles ({ author, topic, sort_by, order, limit, p }) {
-    return connection
-    .select('articles.author', 'articles.title', 'articles.article_id', 'articles.topic', 'articles.created_at', 'articles.votes')
-    .from('articles')
-    .leftJoin('comments', 'articles.article_id', 'comments.article_id')
-    .groupBy('articles.article_id')
-    .count('comments.comment_id as comment_count')
-    .orderBy(sort_by || 'created_at', order || 'desc')
+function fetchArticles({ author, topic, sort_by, order, limit, p }) {
+  return connection
+    .select(
+      "articles.author",
+      "articles.title",
+      "articles.article_id",
+      "articles.topic",
+      "articles.created_at",
+      "articles.votes",
+      "articles.image"
+    )
+    .from("articles")
+    .leftJoin("comments", "articles.article_id", "comments.article_id")
+    .groupBy("articles.article_id")
+    .count("comments.comment_id as comment_count")
+    .orderBy(sort_by || "created_at", order || "desc")
     .modify(query => {
-        if (author) query.where({ 'articles.author': author });
-        if (topic) query.where({ topic });
+      if (author) query.where({ "articles.author": author });
+      if (topic) query.where({ topic });
     })
     .limit(limit || 10)
-    .offset(limit * (p - 1) || 10 * (p - 1) || 0)
-};
+    .offset(limit * (p - 1) || 10 * (p - 1) || 0);
+}
 
-function fetchArticleByID ({ article_id }) {
-    return connection
-    .select('articles.author', 'articles.title', 'articles.article_id', 'articles.body', 'articles.topic', 'articles.created_at', 'articles.votes')
-    .from('articles')
-    .leftJoin('comments', 'articles.article_id', 'comments.article_id')
-    .groupBy('articles.article_id')
-    .count('comments.comment_id as comment_count')
+function fetchArticleByID({ article_id }) {
+  return connection
+    .select(
+      "articles.author",
+      "articles.title",
+      "articles.article_id",
+      "articles.body",
+      "articles.topic",
+      "articles.created_at",
+      "articles.votes",
+      "articles.image"
+    )
+    .from("articles")
+    .leftJoin("comments", "articles.article_id", "comments.article_id")
+    .groupBy("articles.article_id")
+    .count("comments.comment_id as comment_count")
     .modify(query => {
-        if (article_id) query.where({ 'articles.article_id': article_id });
+      if (article_id) query.where({ "articles.article_id": article_id });
     });
-};
+}
 
 function fetchCommentsByArticleID({ article_id, sort_by, order, limit, p }) {
-    return connection
-    .select('comment_id', 'votes', 'created_at', 'author', 'body', 'article_id')
-    .from('comments')
-    .where('article_id', article_id)
-    .orderBy(sort_by || 'created_at', order || 'desc')
+  return connection
+    .select("comment_id", "votes", "created_at", "author", "body", "article_id")
+    .from("comments")
+    .where("article_id", article_id)
+    .orderBy(sort_by || "created_at", order || "desc")
     .limit(limit || 10)
     .offset(limit * (p - 1) || 10 * (p - 1) || 0);
-};
+}
 
-function updateArticle ({ article_id, inc_votes }) {
-    return connection('articles')
-    .where('article_id', article_id)
-    .increment('votes', inc_votes || 0)
-    .returning('*')
-};
+function updateArticle({ article_id, inc_votes }) {
+  return connection("articles")
+    .where("article_id", article_id)
+    .increment("votes", inc_votes || 0)
+    .returning("*");
+}
 
-function postComment ([ article_id, comment ]) {
-    return connection
-    .insert({ author: comment.username, article_id: article_id.article_id, body: comment.body })
-    .into('comments')
-    .returning('*')
-};
+function postComment([article_id, comment]) {
+  return connection
+    .insert({
+      author: comment.username,
+      article_id: article_id.article_id,
+      body: comment.body
+    })
+    .into("comments")
+    .returning("*");
+}
 
-function checkArticle (article_id) {
-    return connection
-    .select('article_id')
-    .from('articles')
-    .where('article_id', article_id)
-    .then((res) => {
-        return res;
+function checkArticle(article_id) {
+  return connection
+    .select("article_id")
+    .from("articles")
+    .where("article_id", article_id)
+    .then(res => {
+      return res;
     });
-};
+}
 
-function addArticle (articleInfo) {
-    return connection
-    .insert({title: articleInfo.title, body: articleInfo.body, author: articleInfo.author, topic: articleInfo.topic })
-    .into('articles')
-    .returning('*');
-};
+function addArticle(articleInfo) {
+  return connection
+    .insert({
+      title: articleInfo.title,
+      body: articleInfo.body,
+      author: articleInfo.author,
+      topic: articleInfo.topic
+    })
+    .into("articles")
+    .returning("*");
+}
 
-function deleteArticle (article_id) {
-    return connection('articles')
-    .where('article_id', article_id)
-    .del('*');
-};
+function deleteArticle(article_id) {
+  return connection("articles")
+    .where("article_id", article_id)
+    .del("*");
+}
 
-module.exports = { fetchArticles, fetchArticleByID, fetchCommentsByArticleID, updateArticle, postComment, checkArticle, addArticle, deleteArticle };
+module.exports = {
+  fetchArticles,
+  fetchArticleByID,
+  fetchCommentsByArticleID,
+  updateArticle,
+  postComment,
+  checkArticle,
+  addArticle,
+  deleteArticle
+};
