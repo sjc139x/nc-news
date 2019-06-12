b20e9bfe8c33e76bee7703263bd3a5f5ace4368c
Samantha Cockerill <samanthajaynecockerill@gmail.com>
2019-06-12 13:20:20 +0100

    adding employer user account

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,32 +1,37 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
-    avatar_url: 'https://i.ibb.co/0yF7bFz/iconfinder-47-456521.png',
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url: "https://i.ibb.co/0yF7bFz/iconfinder-47-456521.png"
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
-    avatar_url: 'https://i.ibb.co/3hCVGwM/iconfinder-38-456512.png',
+    username: "grumpy19",
+    name: "Paul Grump",
+    avatar_url: "https://i.ibb.co/3hCVGwM/iconfinder-38-456512.png"
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
-    avatar_url: 'https://i.ibb.co/w07ZZVr/iconfinder-28-456502.png',
+    username: "happyamy2016",
+    name: "Amy Happy",
+    avatar_url: "https://i.ibb.co/w07ZZVr/iconfinder-28-456502.png"
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
-    avatar_url: 'https://i.ibb.co/m46QDx2/iconfinder-27-456501.png',
+    username: "cooljmessy",
+    name: "Peter Messy",
+    avatar_url: "https://i.ibb.co/m46QDx2/iconfinder-27-456501.png"
   },
   {
-    username: 'weegembump',
-    name: 'Gemma Bump',
-    avatar_url: 'https://i.ibb.co/Dk7RhgV/iconfinder-25-456499.png',
+    username: "weegembump",
+    name: "Gemma Bump",
+    avatar_url: "https://i.ibb.co/Dk7RhgV/iconfinder-25-456499.png"
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
-    avatar_url: 'https://i.ibb.co/9V7WPQk/iconfinder-16-456490.png',
+    username: "jessjelly",
+    name: "Jess Jelly",
+    avatar_url: "https://i.ibb.co/9V7WPQk/iconfinder-16-456490.png"
   },
+  {
+    username: "hire-me-lol",
+    name: "Hire Me Lol",
+    avatar_url: "https://i.ibb.co/3hCVGwM/iconfinder-38-456512.png"
+  }
 ];
